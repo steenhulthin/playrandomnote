961f2c03ae78a544978f3a1ccc8b2f7baeddec66
Steen Hulthin Rasmussen <steenhulthin@gmail.com>
2016-01-17 22:28:57 +0100

    kinda plays a random note now...

--- playrandomnote.js
+++ playrandomnote.js
@@ -1,6 +1,17 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+var notes = [ { 'note' : 'E', 'freq' : '182.41'}, { 'note' : 'A', 'freq' : '110' } ];
+
 context = new AudioContext;
 oscillator = context.createOscillator();
-oscillator.frequency.value = 200;
+
+var randomNumber = getRandomInt(0,2);
+
+oscillator.frequency.value = notes[randomNumber].freq;
+alert(randomNumber + ' ' + notes[randomNumber].note);
 
 oscillator.connect(context.destination);
 
@@ -15,5 +26,5 @@ function stop() {
 
 
 document.getElementById("play").addEventListener("click", play, false);
-document.getElementById("stop").addEventListener("click", stop, false);
+document.getElementById("stop").addEventListener("click", stop , false);
 
