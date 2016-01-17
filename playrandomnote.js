53c8b73d8ab376960b97b92cc69d67a9d13ce7bf
Steen Hulthin Rasmussen <steenhulthin@gmail.com>
2016-01-17 23:16:32 +0100

    stuff.

--- playrandomnote.js
+++ playrandomnote.js
@@ -1,12 +1,14 @@
+(function() {
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
 
-var notes = [ { 'note' : 'E', 'freq' : '182.41'}, { 'note' : 'A', 'freq' : '110' } ];
+var notes = [ { 'note' : 'E', 'freq' : '82.41'}, { 'note' : 'A', 'freq' : '110' } ];
 
 context = new AudioContext;
 oscillator = context.createOscillator();
+oscillator.type = 'square';
 
 var randomNumber = getRandomInt(0,2);
 
@@ -23,8 +25,12 @@ function stop() {
     oscillator.stop(0);
 }
 
-
+function newnote() {
+    //check for started oscillator;
+    //oscillator.frequency.value = notes[randomNumber].freq;
+}
 
 document.getElementById("play").addEventListener("click", play, false);
 document.getElementById("stop").addEventListener("click", stop , false);
-
+document.getElementById("newnote").addEventListener("click", newnote , false);
+} ())
